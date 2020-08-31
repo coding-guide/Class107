93c78a5000ea65106b7ac52b526df53a4c0d32b7
sunrye <srkim8744@korea.kr>
2020-08-31 13:09:06 +0900

    과제해결까지 꼭 스스로 해보세요

--- src/ifstatement/IfExam04.java
+++ src/ifstatement/IfExam04.java
@@ -5,9 +5,15 @@ public class IfExam04 {
 	public static void main(String[] args) {
 // g : ±Ýµµ³¢  s : Àºµµ³¢   b: ¼èµµ³¢  ±×¿Ü : µµ³¢°¡ ¾Æ´Õ´Ï´Ù.
 //		8¿ù 31ÀÏ °úÁ¦ÀÓ. 
-		char ch = 'a';
+		char ch = 'b';
 		if(ch=='g') {
 			System.out.println("±Ýµµ³¢ÀÔ´Ï´Ù.");
+		}else if(ch=='s') {
+			System.out.println("Àºµµ³¢ÀÔ´Ï´Ù.");
+		}else if(ch=='b') {
+			System.out.println("¼èµµ³¢ÀÔ´Ï´Ù.");
+		}else {
+			System.out.println("µµ³¢°¡ ¾Æ´Õ´Ï´Ù.");
 		}
 	}
 
